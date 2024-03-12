04e052b895f5d89d1cbf0eaba273dc49b26b1e6a
wrpereira <wrp.engcivil@hotmail.com>
2024-03-12 18:51:54 -0300

    Resumo - Power BI

--- Queries_SQL.sql
+++ Queries_SQL.sql
@@ -1,13 +1,11 @@
 
-//* ALTERAR TABELA/COLUNA DBO.CHANNEL *//
-UPDATE DBO.channels
+--Alterar conteúdo da coluna dbo.channels
+
+UPDATE dbo.channels
 SET tipo_canal_venda = 'Sistema Interno'
 WHERE tipo_canal_venda = 'OWN CHANNEL'
 
-
-//* ALTERAR TABELA/COLUNA DBO.deliveries *//
-SELECT*
-FROM dbo.deliveries
+--Alterar conteúdo da coluna dbo.deliveries
 
 UPDATE dbo.deliveries
 SET delivery_status = 'Entregue'
@@ -21,51 +19,41 @@ UPDATE dbo.deliveries
 SET delivery_status = 'Cancelado'
 WHERE delivery_status = 'CANCELLED'
 
-
-//* ALTERAR TABELA/COLUNA  DBO.drivers */
-SELECT *
-FROM dbo.drivers
+--Alterar conteúdo da coluna dbo.drivers
 
 UPDATE dbo.drivers
 SET driver_type = 'Operador Logístico'
 WHERE driver_type = 'LOGISTIC OPERATOR'
 
-//* ALTERAR TABELA/COLUNA  DBO.hubs */
+--Alterar tabela da coluna dbo.hubs
 
 SELECT*
-FROM dbo.hubs
-
 ALTER TABLE  dbo.hubs
 DROP COLUMN hub_longitude
 
-
-//* ALTERAR TABELA/COLUNA  dbo.orders *//
-SELECT*
-FROM dbo.orders
+--Alterar tabela da coluna dbo.orders
 
 ALTER TABLE  dbo.orders
 DROP COLUMN order_created_hour;
 
-
 ALTER TABLE  dbo.orders
 DROP COLUMN order_created_minute;
-
 
 ALTER TABLE dbo.orders
 DROP COLUMN order_created_day;
 
-
 ALTER TABLE  dbo.orders
 DROP COLUMN order_created_month;
-
 
 ALTER TABLE  dbo.orders
 DROP COLUMN order_created_year;
 
-//* ALTERAR TABELA/COLUNA  dbo.payments *//
+--Qtde. de meios payment_method
 
-SELECT distinct payment_method
+SELECT DISTINCT payment_method
 FROM dbo.payments
+
+--Alterar conteúdo da coluna dbo.payments
 
 UPDATE dbo.payments
 SET payment_method = 'Cartão de Crédito Loja'
@@ -86,7 +74,6 @@ WHERE payment_method = 'Debit'
 UPDATE dbo.payments
 SET payment_method = 'Cartão de Débito Loja'
 WHERE payment_method = 'DEBIT_STORE'
-
 
 UPDATE dbo.payments
 SET payment_method = 'Dinheiro'
@@ -116,8 +103,6 @@ UPDATE dbo.payments
 SET payment_method = 'Voucher'
 WHERE payment_method = 'VOUCHER'
 
-//* ALTERAR TABELA/COLUNA dbo.payments *//
-
 UPDATE dbo.payments
 SET payment_status = 'AGUARDANDO'
 WHERE payment_status = 'AWAITING'
@@ -126,201 +111,175 @@ UPDATE dbo.payments
 SET payment_status = 'ESTORNADO'
 WHERE payment_status = 'CHARGEBACK'
 
-
-//* ALTERAR TABELA/COLUNA  dbo.hubs *//
-
-SELECT*
-FROM dbo.hubs
-
-
-//*dbo. DRIVERS - Qtde. de categorias de entrega
-2 categorias, motoboy e biker *//
+--Tipos de entregadores
 
 SELECT DISTINCT driver_modal
 FROM dbo.drivers
 
-//*dbo. DRIVERS -Qtde. de funcionarios = 4824 *//
+--Qtde. total de entregadores
 
 SELECT DISTINCT COUNT (cast(driver_id as int)) as 'qtde de entregadores'
 FROM dbo.drivers
 
-//* qtde. de MOTOBOY =  3222  *//
+--Qtde. total de MOTOBOY
 
-SELECT DISTINCT COUNT (cast(driver_id as int)) as 'qtde de entregadores'
+SELECT DISTINCT COUNT (cast(driver_id as int)) as 'qtde total de MOTOBOY'
 FROM dbo.drivers
 WHERE DRIVER_MODAL = 'MOTOBOY'
 
-//* qtde. de BIKERS =  1602 *//
+--Qtde. total de BIKER
 
-SELECT DISTINCT COUNT (cast(driver_id as int)) as 'qtde de entregadores'
+SELECT DISTINCT COUNT (cast(driver_id as int)) as 'qtde total de BIKER'
 FROM dbo.drivers
-WHERE DRIVER_MODAL = 'Biker'
+WHERE DRIVER_MODAL = 'BIKER'
 
-//* QTDE. por regime de contratação = 
-Qtde. de regimes de contratação = Logistic Operator and Freelance *//
+--Tipos de regime de contratação
+
 SELECT DISTINCT driver_type
 FROM dbo.drivers
-//*
-motoboy LO= 884 *//
+
+--Qtde. total de entregadores por regime de contratação
+-- MOTOBOY - Operador Logístico
+
 SELECT COUNT (driver_id)
 FROM dbo.drivers
 WHERE driver_modal = 'MOTOBOY' AND driver_type = 'Operador Logístico'
 
-//*
-motoboy FREE= 2338
-*//
-SELECT COUNT (driver_id)
-FROM dbo.drivers
-WHERE driver_modal = 'MOTOBOY' AND driver_type = 'Freelance'
-
-//* biker LO= 1 *//
+-- MOTOBOY - Freelance
 
 SELECT COUNT (driver_id)
 FROM dbo.drivers
-WHERE driver_modal = 'Biker' AND driver_type = 'Operador Logístico'
+WHERE driver_modal = 'MOTOBOY' AND driver_type = 'FREELANCE'
 
-//* biker FREE= 1601 *//
+-- BIKER - Operador Logístico
 
 SELECT COUNT (driver_id)
 FROM dbo.drivers
-WHERE driver_modal = 'Biker' AND driver_type = 'FREELANCE'
+WHERE driver_modal = 'BIKER' AND driver_type = 'Operador Logístico'
 
-//*dbo. CHANNELS - Qtde. de canais de vendas = 40 *//
+-- BIKER - Operador Logístico
+
+SELECT COUNT (driver_id)
+FROM dbo.drivers
+WHERE driver_modal = 'BIKER' AND driver_type = 'FREELANCE'
+
+--Qtde. total de canais de vendas
 
 SELECT COUNT (CAST(id_canal_venda AS INT)) AS 'Qtde. de canais de vendas'
 FROM dbo.channels
 
-//*dbo. CHANNELS - categorias CANAIS DE VENDAS, se é OWN CHANNEL ou marketplace*//
+--Tipos de categorias de canais de vendas
 
 SELECT DISTINCT tipo_canal_venda
 FROM dbo.channels
 
-//*dbo. CHANNELS - QTDE. marketplace AND OWN CHANNEL*//
-
-//*marketplace= 26 *//
+--Qtde. total de canais de vendas por tipo
+--MARKETPLACE
 
 SELECT DISTINCT COUNT (tipo_canal_venda) 
 FROM dbo.channels
 WHERE tipo_canal_venda = 'MARKETPLACE'
 
-//*Sistema Interno= 14 *//
+--Sistema Interno
 
 SELECT DISTINCT COUNT (tipo_canal_venda) 
 FROM dbo.channels
 WHERE tipo_canal_venda = 'Sistema Interno'
 
+-- Tipos de entregas = Entregue , Cancelado e Rota de Entrega
 
-//*- dbo.DELIVERIES - ENTREGAS = QTDE. ENTREGUES X CANCELADAS*//
-
-//*Tipos de entregas = DELIVERED , CANCELLED ,  DELIVERING *//
 SELECT COUNT (delivery_order_id) , delivery_status
 FROM dbo.deliveries		 
 GROUP BY delivery_status
 
-//*Qtde. total de entregas = 378843 *//
+--Qtde. total de Entregas
 
 SELECT COUNT (delivery_order_id)
 FROM DBO.DELIVERIES
 
-//*Qtde. DELIVERED de entregas feitas = 371367 *//
+-- Qtde. de Entregas Entregue
 
 SELECT COUNT (delivery_status)
 FROM DBO.DELIVERIES
 WHERE delivery_status = 'Entregue'
 
-//*Qtde. CANCELLED de entregas canceladas = 7253 *//
+-- Qtde. de Entregas Canceladas
 
 SELECT COUNT (delivery_status)
 FROM DBO.DELIVERIES
 WHERE delivery_status = 'Cancelado'
 
-//*Qtde. DELIVERING em rota de entrega / ainda não chegaram no cliente = 223 *//
+--Qtde. de Entreags em Rota de Entrega
 
 SELECT COUNT (delivery_status)
 FROM dbo.deliveries
-WHERE delivery_status = 'rota de entrega'
+WHERE delivery_status = 'Rota de Entrega'
 
-//*PORCENTAGEM DE CADA UMA DELAS EM RELAÇÃO AO TOTAL *//
-//* % de todas juntas na mesma query*//
+-- % de representatividade de cada tipo de status de entrega em relação ao total de Entregas
 
 SELECT COUNT(p.delivery_id) , p.delivery_status,
-cast( COUNT(p.delivery_id) / tmp.total *100.00 as numeric (10,2))
-FROM dbo.deliveries p , (select cast (count(delivery_id) as numeric (10,2)) total FROM dbo.deliveries) tmp
+CAST( COUNT(p.delivery_id) / tmp.total *100.00 as numeric (10,2))
+FROM dbo.deliveries p , (SELECT CAST (COUNT(delivery_id) as numeric (10,2)) total FROM dbo.deliveries) tmp
 GROUP BY p.delivery_status, tmp.total
 
-//* % de Delivered em relação ao total*//
+-- % de Entregas com status Entregue em relação ao total
 
 SELECT COUNT(p.delivery_id) , p.delivery_status,
-cast( COUNT(p.delivery_id) / tmp.total *100.00 as numeric (10,2))
-FROM dbo.deliveries p , (select cast (count(delivery_id) as numeric (10,2)) total FROM dbo.deliveries) tmp
+CAST( COUNT(p.delivery_id) / tmp.total *100.00 as numeric (10,2))
+FROM dbo.deliveries p , (SELECT CAST (COUNT(delivery_id) as numeric (10,2)) total FROM dbo.deliveries) tmp
 WHERE p.delivery_status = 'Entregue'
 GROUP BY p.delivery_status, tmp.total
 
-//* % de Cancelled em relação ao total*//
+-- % de Entregas com status cancelado em relação ao total
 
-SELECT COUNT (P.delivery_id), P.delivery_status,
-CAST(COUNT(P.delivery_id) / tmp.total *100.00 AS NUMERIC (10,2))
-FROM dbo.deliveries P,
-(SELECT CAST (COUNT	(deliverY_id) AS NUMERIC (10,2)) total
-FROM dbo.deliveries) tmp
-WHERE P.delivery_status = 'Cancelado'
+SELECT COUNT(p.delivery_id) , p.delivery_status,
+CAST( COUNT(p.delivery_id) / tmp.total *100.00 as numeric (10,2))
+FROM dbo.deliveries p , (SELECT CAST (COUNT(delivery_id) as numeric (10,2)) total FROM dbo.deliveries) tmp
+WHERE p.delivery_status = 'Cancelado'
 GROUP BY p.delivery_status, tmp.total
 
-//* % de Delivering em relação ao total*//
+-- % de Entregas com status Rota de Entrega em relação ao total
 
-SELECT COUNT (P.delivery_id), P.delivery_status,
-CAST(COUNT(P.delivery_id) / tmp.total *100.00 AS NUMERIC (10,2))
-FROM dbo.deliveries P,
-(SELECT CAST (COUNT	(deliverY_id) AS NUMERIC (10,2)) total
-FROM dbo.deliveries) tmp
-WHERE P.delivery_status = 'Rota de Entrega'
+SELECT COUNT(p.delivery_id) , p.delivery_status,
+CAST( COUNT(p.delivery_id) / tmp.total *100.00 as numeric (10,2))
+FROM dbo.deliveries p , (SELECT CAST (COUNT(delivery_id) as numeric (10,2)) total FROM dbo.deliveries) tmp
+WHERE p.delivery_status = 'Rota de Entrega'
 GROUP BY p.delivery_status, tmp.total
 
+--Qtde. total de HUBS
 
-//* dbo.HUBS - LOCAL ONDE TEM UMA STORE E CADA STORE TEM SEU NOME E CADA HUB TAMBÉM
- - LEGAL FAZER UM FILTRO NO POWER COM OS HUBS *//
-
-//*Qtde. de HUBS = 32 e quais são *//
 SELECT COUNT (hub_id) , hub_name
 FROM dbo.hubs
 group by hub_name
 
-//* dbo.orders - qtde vendas, faturamento, *//
-
-//* quantidade de ORDERS / VENDAS*//
+--Qtde. total de Orders / Pedidos
 
 SELECT COUNT (order_id)
 FROM dbo.orders
 
+--Faturamento total de acordo com o total de pagamentos e status de pagamento
 
-//* Faturamento total de acordo com o payment e status *//
-
-SELECT payment_status , SUM (CAST (payment_amount AS DECIMAL (10,2))) as Faturamento
+SELECT payment_status , SUM (CAST (payment_amount as decimal (10,2))) as Faturamento
 FROM dbo.payments
 GROUP BY payment_status
 
+--Total de impostos pagos pelos clientes de acordo com o status do pagamento
 
-//* Total de impostos pagos pelos clientes de acordo com o status do pagamento *//
-
-SELECT payment_status , SUM (CAST (payment_fee AS REAL)) as Impostos
+SELECT payment_status , SUM (CAST (payment_fee as real)) as Impostos
 FROM dbo.payments
 GROUP BY payment_status
 
+--Tipos de meios de pagamentos
 
-//*dbo.payments categorias / tipos de meios de pagamentos compras cenladaas e finalizadas, tudo junto, status do pagamento *//
-
-//*meios de pagamentos*//
 SELECT DISTINCT payment_method
 FROM dbo.payments
 
-//*payments_status
-3 tipos = pago , aguardando , estornado *//
+--Tipos de status de pagamento
 
 SELECT DISTINCT payment_status
 FROM dbo.payments
 
-//*quantidade de vendas por cada meio de pagamento e se foi finalizada ou canceladas*//
-//* ORDER AMOUNT =ORDERS , MEIO DE PAGAMENTO = PAYMENTS, STATUS DA COMPRA - ORDERS , STATUS DO PAGAMENTO PAYMENTS*//
+--Resumo de status do pedido, meio de pagamento,  status de pagamento e valor pago
 
 SELECT  P.payment_order_id, order_status , payment_method , payment_status, payment_amount  
 FROM dbo.orders P
